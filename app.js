b6dd80f3d49ce8b489245805550b40e92dde5a9c
Alex Nasonov <nasonov.alexey@gmail.com>
2016-06-16 15:36:46 +0300

    Deploy

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
@@ -32,8 +31,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
--- routes/main.js
+++ routes/main.js
@@ -4,13 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('main', {page: "home", title: "International Kung-Fu Festival"});
+    res.render('main', {lang: "en", title: "International Kung-Fu Festival"});
 });
 
 
 /* LOGOUT */
 router.get('/logout', function (req, res) {
-    req.logout();
     res.redirect('/');
 });
 
